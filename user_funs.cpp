1f4157b8de17fd33c03a1e3275ae63357bebedb2
Bojownikjpjpjpjp <rzygamteczajp@gmail.com>
2022-11-12 15:11:00 +0100

    user func update

--- user_funs.cpp
+++ user_funs.cpp
@@ -1,2 +1,35 @@
 #include"user_funs.h"
 
+matrix ff1T(matrix x, matrix ud1, matrix ud2)
+{
+	matrix y;
+	y = -cos(0.1 * x()) * exp(-pow(0.1 * x() - 2 * 3.14, 2)) + 0.002 * pow(0.1 * x(), 2);
+	return y;
+}
+
+matrix ff1R(matrix x, matrix ud1, matrix ud2)
+{
+	matrix y;
+	matrix Y0 = matrix(3, new double[3] { 5, 1, 10 });
+	matrix* Y = solve_ode(df1, 0, 1, 1000, Y0, ud1, x);
+	int n = get_len(Y[0]);
+	double max = Y[1](0, 2);
+	for (int i = 1; i < n; ++i)
+		if (max < Y[1](i, 2))
+			max = Y[1](i, 2);
+	y = abs(max - 40);
+	return y;
+}
+
+
+matrix df1(double t, matrix Y, matrix ud1, matrix ud2)
+{
+	double a = 0.98, b = 0.63, g = 9.81, PA = 1, TA = 90, PB = 1, DB = 0.00365665, Fin = 0.01, Tin = 10;
+	matrix dY(3, 1);
+	double FAout = Y(0) > 0 ? a * b * m2d(ud2) * sqrt(2 * g * Y(0) / PA) : 0;
+	double FBout = Y(1) > 0 ? a * b * DB * sqrt(2 * g * Y(1) / PB) : 0;
+	dY(0) = -FAout;
+	dY(1) = FAout + Fin - FBout;
+	dY(2) = Fin / Y(1) * (Tin - Y(2)) + FAout / Y(1) * (TA - Y(2));
+	return dY;
+}
--- user_funs.h
+++ user_funs.h
@@ -2,4 +2,6 @@
 
 #include"ode_solver.h"
 
-
+matrix ff1T(matrix, matrix = NAN, matrix = NAN);
+matrix ff1R(matrix, matrix = NAN, matrix = NAN);
+matrix df1(double, matrix, matrix = NAN, matrix = NAN);
