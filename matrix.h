ef83ba7a65eaaace795daa02e05fc9debbb7fbab
Bojownikjpjpjpjp <rzygamteczajp@gmail.com>
2022-11-12 15:01:11 +0100

    dodanie komentarzy do nagłówkowych matrix i solution

--- matrix.h
+++ matrix.h
@@ -16,51 +16,134 @@ class matrix
 {
 	int n, m;
 	double** M;
+
+#pragma region funkcje_zaprzyjaŸnione
+	//zwraca wymiary macierzy n oraz m
 	friend int* get_size(const matrix&);
+
+	//zwraca d³ugoœæ wektora pionowego n (macierzy nx1) 
 	friend int get_len(const matrix&); // throw (string);
+#pragma endregion
+
 public:
+	//Zmienna typu double, jak równie¿ zmienna typu int, jest konwertowana na macierz 1x1!
+#pragma region konstruktory
+	//tworzy macierz 1x1 o podanej wartoœci. Jest równie¿ wykorzystywanyjako konstruktor konwertuj¹cy(zamienia m.in. double i int na matrix)
 	matrix(double = 0.0);
+
+	//tworzy macierz nxm, której ka¿dy element jest równy trzeciemu argumentowi
 	matrix(int, int, double = 0.0); // throw (string);
+
+	//tworzy macierz nx1 (wektor pionowy) i wype³nia wartoœciami podanymi w tablicy 1D
 	matrix(int, double*); // throw (string);
+
+	//tworzy macierz nxm i wype³nia wartoœciami podanymi w tablicy 2D
 	matrix(int, int, double**); // throw (string);
+
+	//konstruktor kopiuj¹cy
 	matrix(const matrix&);
+	//destruktor
 	~matrix();
+#pragma endregion
+
+	//Sk³adowe funkcje operatorowe: 
+#pragma region operatory
+	//operator przypisania
 	matrix& operator=(const matrix&);
+
+	//zwraca wskazan¹ kolumnê macierzy w postaci macierzy nx1 (rezultat zwracany jest przez wartoœæ->u¿ycie operatora nie jest l - wartoœci¹)
 	matrix operator[](int) const; // throw (string);
+
+	// operatory zwracaj¹ wybrany element macierzy(mog¹ byæ wykorzystane do konwersji matrix na double)
 	double& operator()(int = 0, int = 0); // throw (string);
 	double operator()(int = 0, int = 0) const; // throw (string);
+#pragma endregion
+
+#pragma region funkcje_sk³adowe
+	//wstawia we wskazan¹ kolumnê wektor pionowy (macierz nx1)
 	void set_col(const matrix&, int); // throw (string);
+
+	//wstawia we wskazany wiersz wektor poziomy (macierz 1xm)
 	void set_row(const matrix&, int); // throw (string);
+
+	//dodaje do macierzy now¹ kolumnê i wype³nia j¹ podan¹ wartoœci¹
 	void add_col(double = 0.0);
+
+	//dodaje do macierzy nowywiersz i wype³nia go podan¹ wartoœci¹
 	void add_row(double = 0.0);
+
+	//dodaje do macierzy now¹ kolumnê i wstawia w ni¹ podany wektor pionowy(macierz nx1)
 	void add_col(const matrix&); // throw (string);
+
+	//dodaje do macierzy nowy wiersz i wstawie w niego podany wektor poziomu(macierz 1xm)
 	void add_row(const matrix&); // throw (string);
+#pragma endregion
 };
 
+#pragma region globalne_funkcje_operatorowe
+//operatory wykonuj¹ odpowiednie dzia³anie pod warunkiem zgodnoœci wymiarów macierzy
 matrix operator+(const matrix&, const matrix&); // throw (string);
 matrix operator-(const matrix&, const matrix&); // throw (string);
 matrix operator*(const matrix&, const matrix&); // throw (string);
 matrix operator/(const matrix&, const matrix&); // throw (string);
+
+//macierz przeciwna
 matrix operator-(const matrix&);
+
+//operatory relacji dzia³aj¹ tylko dla macierzy 1x1
 bool operator<(const matrix&, const matrix&); // throw (string);
 bool operator>(const matrix&, const matrix&); // throw (string);
 bool operator<=(const matrix&, const matrix&); // throw (string);
 bool operator>=(const matrix&, const matrix&); // throw (string);
 bool operator==(const matrix&, const matrix&); // throw (string);
 bool operator!=(const matrix&, const matrix&); // throw (string);
-matrix ident_mat(int = 1); // throw (string);
-matrix rand_mat(int = 1, int = 1); // throw (string);
-matrix randn_mat(int = 1, int = 1); // throw (string);
-double m2d(const matrix&); // throw (string);
-double det(const matrix&); // throw (string);
-matrix inv(const matrix&); // throw (string);
-matrix trans(const matrix&);
-matrix pow(const matrix&, int = 2); // throw (string);
-double norm(const matrix&); // throw (string);
-matrix hcat(const matrix&, const matrix&); // throw (string);
-matrix vcat(const matrix&, const matrix&); // throw (string);
-matrix get_col(const matrix&, int); // throw (string);
-matrix get_row(const matrix&, int); // throw (string);
+
+//wypisanie macierzy na ekran lub do pliku csv.Podczas wypisywani elementów macierzy, czêœæ ca³kowita oddzielona jest od czêœci		u³amkowej za pomoc¹ zdefiniowanego w linii 13 SEP_SYMBOL
 ostream& operator<<(ostream&, const matrix&);
+
+//odczyt macierzy z pliku csv, txt lub klawiatury.Podaj¹c elementy macierzy nale¿y pamiêtaæ, ¿e ka¿da liczba musi koñczyæ siê znakiem; Podczas	wprowadzania elementów macierzy, czêœæ ca³kowita mo¿e byæ oddzielona od czêœci u³amkowej za pomoc¹ zdefiniowanego w linii 13 SEP_SYMBOL lub . 
 istream& operator>>(istream&, matrix&); // throw (string);
+#pragma endregion
+
+#pragma region funkcje_globalne
+//tworzy macierz jednostkow¹ nxn 
+matrix ident_mat(int = 1); // throw (string);
+
+//tworzy macierz nxm i wype³nia wartoœciami losowymi z przedzia³u[0, 1] o rozk³adzie jednostajnym
+matrix rand_mat(int = 1, int = 1); // throw (string);
+
+//tworzy macierz nxm i wype³nia wartoœciami	losowymi o standardowym rozk³adzie normalnym
+matrix randn_mat(int = 1, int = 1); // throw (string);
+
+//zamiana macierzy 1x1 na double
+double m2d(const matrix&); // throw (string);
+
+//wyznacznik macierzy
+double det(const matrix&); // throw (string);
+
+// macierz odwrotna
+matrix inv(const matrix&); // throw (string);
+
+//– macierz transponowana
+matrix trans(const matrix&);
+
+//podnosi macierz do potêgi domyœlnie 2
+matrix pow(const matrix&, int = 2); // throw (string);
+
+//oblicza normê z wektora pionowego (macierzy nx1)
+double norm(const matrix&); // throw (string);
+
+//poziome po³¹czenie dwóch macierzy
+matrix hcat(const matrix&, const matrix&); // throw (string);
+
+//pionowe po³¹czenie dwóch macierzy
+matrix vcat(const matrix&, const matrix&); // throw (string);
+
+//zwraca wskazan¹ kolumnê macierzy w postaci wektora pionowego(macierzy nx1)
+matrix get_col(const matrix&, int); // throw (string);
+
+//zwraca wskazany wiersz macierzy w postaci wektora	poziomego(macierzy 1xm)
+matrix get_row(const matrix&, int); // throw (string);
+#pragma endregion
+
 
--- solution.h
+++ solution.h
@@ -7,25 +7,70 @@
 class solution
 {
 public:
+#pragma region sk³adowe_obiektu
+	//macierz(najczêœciej nx1) zawieraj¹ca wspó³rzêdne punktu stanowi¹ce rozwi¹zanie
 	matrix x;
+
+	//– macierz (najczêœciej 1x1) zawieraj¹ca wartoœæ funkcji celu w punkcie x
 	matrix y;
+
+	// macierz nx1 zawieraj¹ca gradient funkcji celu w punkcie x
 	matrix g;
+
+	// macierz nxn zawieraj¹ca hesjan funkcji celu w punkcie x
 	matrix H;
+
+	//used data – macierz do dyspozycji u¿ytkownika (used data) 
 	matrix ud;
+
+	//liczba ca³kowita zawieraj¹ca informacjê o przyczynie zakoñczenia poszukiwania minimum funkcji celu.
 	int flag;
+
+	//zmienna statyczna zawieraj¹ca liczbê wywo³añ funkcji celu
 	static int f_calls;
+
+	//zmienna statyczna zawieraj¹ca liczbê obliczeñ gradientu funkcji celu
 	static int g_calls;
+
+	//zmienna statyczna zawieraj¹ca liczbê obliczeñ hesjanu funkcji celu 
 	static int H_calls;
+#pragma endregion
+
+	//funkcja statyczna zeruj¹ca zmienne f_calls, h_calls oraz H_calls
 	static void clear_calls();
+
+#pragma region konstruktory
+	//tworzy rozwi¹zanie o przes³anej wspó³rzêdnej
 	solution(double = NAN);
+
+	// tworzy rozwi¹zanie o przes³anych wspó³rzêdnych
 	solution(const matrix&);
+
+	//tworzy rozwi¹zanie o przes³anych wspó³rzêdnych
 	solution(int, double*); // throw (string);
+
+	//konstruktor kopiuj¹cy (macierz ud nie jest kopiowana je¿eli w	obiekcie wzorcowym ud(0, 0) ma wartoœæ NAN)
 	solution(const solution&);
+#pragma endregion
+
+	//operator przypisania (macierz ud nie jest	przypisywana je¿eli w obiekcie wzorcowym ud(0, 0) ma wartoœæ NAN)
 	solution& operator=(const solution&);
+
+#pragma region funkcje_sk³adowe
+	//funkcja wyznacza wartoœæ zmiennej y poprzez wywo³anie funkcji, której adres jest pierwszym argumentem oraz zwiêksza f_calls
 	matrix fit_fun(matrix(*)(matrix, matrix, matrix), matrix = NAN, matrix = NAN); // throw (string);
+
+	//funkcja wyznacza wartoœæ zmiennej g poprzez wywo³anie funkcji, której adres jest pierwszym argumentem oraz zwiêksza g_calls
 	matrix grad(matrix(*)(matrix, matrix, matrix), matrix = NAN, matrix = NAN); // throw (string);
+
+	//funkcja wyznacza wartoœæ zmiennej H poprzez wywo³anie funkcji, której adres jest pierwszym argumentem oraz zwiêksza H_calls
 	matrix hess(matrix(*)(matrix, matrix, matrix), matrix = NAN, matrix = NAN); // throw (string);
+#pragma endregion
+
 };
 
+//funkcja zwraca d³ugoœæ wektora (macierzy nx1) x
 int get_dim(const solution&); // throw (string);
+
+//wypisanie rozwi¹zania na ekran (wartoœci g_calls i H_calls s¹ wypisywane tylko wtedy, gdy s¹ wiêksze od 0).
 ostream& operator<<(ostream&, const solution&);
